be9afd9e588a5c0a2ef643c8b6874d139a43e07e
Ali Arab <aliarab812@yahoo.com>
2021-05-15 04:56:46 -0500

    blog routes added to index

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const blogRoutes = require('./blog_routes');
 
 router.use('/api', apiRoutes);
+router.use(blogRoutes)
 
 module.exports = router;
