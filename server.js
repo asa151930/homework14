373efd5cbe2fac382e1fd4e5155c29e94eee0ed0
Ali Arab <aliarab812@yahoo.com>
2021-05-14 21:41:19 -0500

    set up

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/homeRoutes.js');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('');
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'public')));
+// Sets up the routes
+app.use(require('./controllers/dish-routes'));
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:' + PORT);
+});
+
