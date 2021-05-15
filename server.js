25a770f9ac20f2b8b36552a21e26b56955d02373
Ali Arab <aliarab812@yahoo.com>
2021-05-15 02:14:55 -0500

    static public added

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(routes);
 
