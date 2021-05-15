1d591f0b595aa860c73791afcfd42b1f2121cb59
Ali Arab <aliarab812@yahoo.com>
2021-05-15 00:10:52 -0500

    folder and files added from mini project

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-// Dependencies
 const express = require('express');
-const path = require('path');
-const routes = require('./controller/homeRoutes.js');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('');
+const session = require('express-session');
+const routes = require('./controllers');
 
-// Sets up the Express App
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
-// Sets up the routes
-app.use(require('./controllers/dish-routes'));
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
-
